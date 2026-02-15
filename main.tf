4dd9ee809d7aafcdd2fedafc44a8a99b3349d8ea
akshaykoli05 <koliakshay05@gmail.com>
2026-02-15 12:10:08 +0530

    Initial commit: root config + resource_group module

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  cloud {
+    organization = "akshayInfra"   # Replace with your Terraform Cloud org name
+
+    workspaces {
+      name = "Dev"    # Replace with your workspace name
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "rg" {
+  source   = "./modules/resource_group"
+  name     = "my-tf-rg"
+  location = "East US"
+  tags = {
+    environment = "dev"
+    owner       = "teamA"
+  }
+}
